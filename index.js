d1dfbbabce7a3676cd709875be8217ebfbd796a4
vikash-mandal747 <vikashmandal747@gmail.com>
2025-08-18 19:28:35 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require('dotenv').config();
+var cors = require('cors');
 const DBConnection = require("./config/db");
 const userRouter = require("./routes/user.routes");
 const noteRouter = require("./routes/note.routes");
@@ -9,6 +10,8 @@ const noteRouter = require("./routes/note.routes");
 
 const app = express()
 app.use(express.json())
+
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("home page")
